8451e3a9c7b7262f89af3f3aa5b6c17c1da95561
Andre Katov <andy.katov@icloud.com>
2022-10-07 20:27:13 +0300

    added finished assignment

--- Ex1.1--Yes-or-No/app.js
+++ Ex1.1--Yes-or-No/app.js
@@ -1,1 +1,11 @@
 //? Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function yesOrNo(bool) {
+  bool === true
+    ? console.log("Yes")
+    : bool === false
+    ? console.log("No")
+    : console.log("not a boolean");
+}
+
+yesOrNo(false);
