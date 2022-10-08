b95cf29b7e9110e2b6eb31d2b62765443a058b0d
Andre Katov <andy.katov@icloud.com>
2022-10-08 18:32:48 +0300

    finished assignment

--- Ex2.6--Years-and-Centuries/app.js
+++ Ex2.6--Years-and-Centuries/app.js
@@ -1,4 +1,5 @@
-//? The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+//? The first century spans from the year 1 up to and including the year 100,
+//? The second - from the year 101 up to and including the year 200, etc.
 //? Task :
 //? Given a year, return the century it is in.
 //? Input , Output Examples ::
@@ -6,3 +7,10 @@
 //? centuryFromYear( 1900) returns (19)
 //? centuryFromYear(1601) returns (17)
 //? centuryFromYear(2000) returns (20)
+
+function centuryFromYear(year) {
+  year % 100 === 0 ? (year /= 100) : (year = Math.floor(year / 100) + 1);
+  return year;
+}
+
+console.log(centuryFromYear(1));
--- Ex2.7--Basic-Math/app.js
+++ Ex2.7--Basic-Math/app.js
@@ -1,4 +1,4 @@
-//? Your task is to create a function that does four basic .
+//? Your task is to create a function that does four basic math calculations.
 //? The function should take three arguments - operation(string/char), value1(number), value2(number).
 //? The function should return result of numbers after applying the chosen operation.
 //? Examples:
@@ -6,3 +6,46 @@
 //? basicOp('-', 15, 18)   // Output: -3
 //? basicOp('*', 5, 5)   // Output: 25
 //? basicOp('/', 49, 7)   // Output: 7
+
+// Operation functions.
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+//Main calc func
+function calculator(num1, operator, num2) {
+  let charCode = operator.charCodeAt();
+  if (
+    (charCode !== 47 &&
+      charCode !== 43 &&
+      charCode !== 42 &&
+      charCode !== 45) ||
+    operator.split("").length > 1
+  ) {
+    return `Not valid operator. Operator must be in double quotes like so "+" "-" "*" "/"`;
+  } else {
+    if (operator === "+") {
+      return add(num1, num2);
+    }
+    if (operator === "-") {
+      return subtract(num1, num2);
+    }
+    if (operator === "*") {
+      return multiply(num1, num2);
+    }
+    if (operator === "/") {
+      return divide(num1, num2);
+    }
+  }
+}
+
+console.log(calculator(100, "*", 20));
