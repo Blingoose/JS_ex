84b2c6108660b1885d46b141873b2324cc0f2437
Andre Katov <andy.katov@icloud.com>
2022-10-10 02:04:39 +0300

    finished

--- Ex5.1--trimming-string/app.js
+++ Ex5.1--trimming-string/app.js
@@ -1,3 +1,19 @@
 //? It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
 //? You're given one parameter, the original string.
 //? You don't have to worry with strings with less than two characters.
+
+//Test.
+// const testString = "Hello";
+// const result = testString.substring(1, testString.length - 1);
+// console.log(result);
+
+function trimStr(str) {
+  if (typeof str !== "string" || str) {
+    return "Invalid input: empty trimStr() or non Strings not allowed.";
+  } else if (str.length <= 1) {
+    return "Must contain a string of 2 or more characters.";
+  }
+  return str.slice(1, -1);
+}
+
+console.log(trimStr("*Hello World*"));
--- Ex5.2--String-Repeat/app.js
+++ Ex5.2--String-Repeat/app.js
@@ -1,3 +1,27 @@
 //? Write a function called repeat_str which repeats the given string src exactly count times.
 //? repeatStr(6, "I") // "IIIIII"
 //? repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+// Version 2.
+// function repeatString2(str, n) {
+//   let repeated = "";
+//   for (let i = 0; i < n; i++) {
+//     repeated += str;
+//   }
+//   return repeated;
+// }
+// console.log(repeatString2("Yo", 8));
+
+function repeatString(str, n = 1) {
+  if (
+    typeof str !== "string" ||
+    typeof n !== "number" ||
+    str.length < 1 ||
+    n <= 0
+  ) {
+    return "Invalid input. Example of valid input ---> ('string', n=8)  n > 0";
+  }
+  return str.repeat(n);
+}
+
+console.log(repeatString("Out-Get", (n = 10)));
