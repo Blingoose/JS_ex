fbe0a11184536b5219d0f25511beb7c104297a8a
Andre Katov <andy.katov@icloud.com>
2022-10-10 16:42:42 +0300

    Added code to handle edge cases

--- Ex5.3--To-Camel-Case/app.js
+++ Ex5.3--To-Camel-Case/app.js
@@ -16,12 +16,18 @@ function camelCase(str) {
   }
   const check = ["-", "_"];
   const firstLetter = str.charAt(0);
-  const word = str.toLowerCase().split("");
-  //checking for dash/underscore and replacing them.
+  let word = str.toLowerCase().split("");
+  let count = 0;
+  //checking for dash/underscore and replacing them. If no dash/underscore found, print the word as is.
   for (let i = 1; i < str.length; i++) {
     if (check.indexOf(str[i]) > -1) {
       word[i] = "";
       word[i + 1] = "" + word[i + 1].toUpperCase();
+    } else if (check.indexOf(str[i]) < 0) {
+      count++;
+      if (count === str.length - 1) {
+        return "Word does not contain dashes(-) or underscores(_).";
+      }
     }
   }
   //checking the first letter for upper/lower & making sure that non A-Za-z aren't used.
@@ -40,5 +46,8 @@ function camelCase(str) {
 }
 console.log(camelCase("The_Stealth_Warrior"));
 console.log(camelCase("wHAT-IS_UP-HOMMIE"));
+console.log(camelCase("_No-Dashes at the beginning"));
 console.log(camelCase("@453he_Stealth_Warrior"));
 console.log(camelCase());
+console.log(camelCase("HElloWorld"));
+console.log(camelCase("HEllo World"));
